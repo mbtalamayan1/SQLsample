efb9e9371ac489224386691a009c292c852b2e31
mbtalamayan1 <marcbenedicttalamayan@gmail.com>
2023-08-10 15:57:29 +0800

    Added supplementary SQL files

---
+++ customers.sql
@@ -0,0 +1,375 @@
+﻿/* CREATE TABLE */
+CREATE TABLE customers(
+  customer_id INT, 
+  first_name VARCHAR(100), 
+  last_name VARCHAR(100), 
+  gender VARCHAR(100), 
+  email VARCHAR(100), 
+  city VARCHAR(100), 
+  country VARCHAR(100)
+);
+
+INSERT INTO customers VALUES (
+    1, 'Robinetta', 'Duckworth', 'Female', 
+    'rduckworth6@51.la', 'Quezon City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    2, 'Wendeline', 'Le Clercq', 'Female', 
+    'wleclercq8@yellowbook.com', 'Marikina City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    3, 'Albina', 'Smartman', 'Female', 
+    'asmartman9@creativecommons.org', 
+    'Mandaluyong City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    4, 'Julieta', 'De Ruggiero', 'Female', 
+    'jderuggieroa@ihg.com', 'Pasig City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    5, 'Selinda', 'Arrowsmith', 'Female', 
+    'sarrowsmithb@phpbb.com', 'Makati City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    6, 'Donavon', 'McIlory', 'Male', 'dmciloryi@bloglines.com', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    7, 'Darci', 'Souter', 'Female', 'dsouterj@cdc.gov', 
+    'Makati City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    8, 'Neddy', 'Suff', 'Male', 'nsuffo@cyberchimps.com', 
+    'Mandaluyong City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    9, 'Cybil', 'Pymm', 'Female', 'cpymmp@infoseek.co.jp', 
+    'Manila City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    10, 'Lia', 'Syddon', 'Female', 'lsyddonq@samsung.com', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    11, 'Arlyn', 'Duckerin', 'Female', 
+    'aduckerins@alibaba.com', 'Mandaluyong City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    12, 'Glenden', 'Antunez', 'Male', 'gantunezt@nps.gov', 
+    'Manila City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    13, 'Trula', 'Boome', 'Female', 'tboomeu@cocolog-nifty.com', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    14, 'Bobette', 'Orehead', 'Female', 
+    'boreheadx@tiny.cc', 'Pasig City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    15, 'Lilyan', 'Roggieri', 'Female', 
+    'lroggieriz@amazon.de', 'Quezon City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    16, 'Blinnie', 'Martinet', 'Female', 
+    'bmartinet10@eepurl.com', 'Makati City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    17, 'Jarret', 'Swayland', 'Male', 'jswayland1e@istockphoto.com', 
+    'Taguig City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    18, 'Pearce', 'Barth', 'Male', 'pbarth1g@nyu.edu', 
+    'Pasig City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    19, 'Pietra', 'Latch', 'Female', 'platch1h@cbslocal.com', 
+    'Manila City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    20, 'Burk', 'Prayer', 'Male', 'bprayer1k@uiuc.edu', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    21, 'Reynold', 'Andrioli', 'Male', 
+    'randrioli1m@miibeian.gov.cn', 
+    'Marikina City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    22, 'Dallis', 'Robelow', 'Male', 'drobelow1q@ca.gov', 
+    'Makati City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    23, 'Bobette', 'McCallion', 'Female', 
+    'bmccallion1v@chicagotribune.com', 
+    'Pasig City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    24, 'Harriett', 'Atkin', 'Female', 
+    'hatkin1w@goodreads.com', 'Pasig City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    25, 'Luce', 'Peek', 'Female', 'lpeek1x@twitter.com', 
+    'Manila City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    26, 'Fredek', 'Stearn', 'Male', 'fstearn1y@xinhuanet.com', 
+    'Makati City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    27, 'Kathy', 'Walles', 'Female', 'kwalles20@accuweather.com', 
+    'San Juan City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    28, 'Dasie', 'Beggio', 'Female', 'dbeggio24@reverbnation.com', 
+    'Pasig City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    29, 'Archaimbaud', 'Chattelaine', 
+    'Male', 'achattelaine2d@japanpost.jp', 
+    'Makati City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    30, 'Nikaniki', 'Bladen', 'Female', 
+    'nbladen2g@cam.ac.uk', 'Quezon City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    31, 'Gianna', 'Fidal', 'Female', 'gfidal2i@ucla.edu', 
+    'Mandaluyong City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    32, 'Thain', 'Abbotts', 'Male', 'tabbotts2n@purevolume.com', 
+    'Pasay City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    33, 'Symon', 'Smythe', 'Male', 'ssmythe2o@shareasale.com', 
+    'Makati City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    34, 'Salome', 'Blackstone', 'Female', 
+    'sblackstone2v@sbwire.com', 'Quezon City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    35, 'Roseanna', 'Risbrough', 'Female', 
+    'rrisbrough2x@spiegel.de', 'Quezon City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    36, 'Freeland', 'Di Carlo', 'Male', 
+    'fdicarlo2y@gmpg.org', 'Taguig City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    37, 'Emmye', 'Beardon', 'Female', 'ebeardon31@hatena.ne.jp', 
+    'Manila City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    38, 'Sheffy', 'Binne', 'Male', 'sbinne32@shinystat.com', 
+    'Manila City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    39, 'Zaria', 'Beatey', 'Female', 'zbeatey34@myspace.com', 
+    'Makati City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    40, 'Tamara', 'Wethey', 'Female', 'twethey35@istockphoto.com', 
+    'Taguig City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    41, 'Beltran', 'Orford', 'Male', 'borford38@yahoo.co.jp', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    42, 'Arnaldo', 'McNeil', 'Male', 'amcneil39@tamu.edu', 
+    'Pasig City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    43, 'Augustina', 'Bethel', 'Female', 
+    'abethel3l@i2i.jp', 'Makati City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    44, 'Bell', 'Sygrove', 'Female', 'bsygrove3q@livejournal.com', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    45, 'Carrissa', 'Brogini', 'Female', 
+    'cbrogini3r@xinhuanet.com', 'San Juan City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    46, 'Valera', 'Ekless', 'Female', 'vekless3s@wsj.com', 
+    'Pasay City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    47, 'Levon', 'Brafferton', 'Male', 
+    'lbrafferton3u@biblegateway.com', 
+    'San Juan City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    48, 'Joannes', 'Maytum', 'Female', 
+    'jmaytum3v@timesonline.co.uk', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    49, 'Prent', 'Stormes', 'Male', 'pstormes3x@redcross.org', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    50, 
+    'Baldwin', 
+    'Hingerty','Male','bohingerty40@shutterfly.com','Makati City','Philippines');
+INSERT INTO customers VALUES( 51,'Shalom','Habeshaw','Male','shabeshaw42@msn.com','Taguig City','Philippines');
+INSERT INTO customers VALUES( 52,'Jeffry','Bending','Male','jbending49@census.gov','Manila City','Philippines');
+INSERT INTO customers VALUES( 53,'Pryce','McMullen','Male','pmcmullen4b@netscape.com','Pasig City','Philippines');
+INSERT INTO customers VALUES( 54,'Bernadine','Carroll','Female','bcarroll4g@state.tx.us','Taguig City','Philippines');
+INSERT INTO customers VALUES( 55,'Angelle','Wager','Female','awager4l@hibu.com','Makati City','Philippines');
+INSERT INTO customers VALUES( 56,'Drucie','Senner','Female','dsenner4m@privacy.gov.au','Mandaluyong City','Philippines');
+INSERT INTO customers VALUES( 57,'Gabriello','Walburn','Male','gwalburn50@t.co','Quezon City','Philippines');
+INSERT INTO customers VALUES( 58,'Trenton','Lurcock','Male','tlurcock58@cpanel.net','Taguig City','Philippines');
+INSERT INTO customers VALUES( 59,'Ignazio','Biasini','Male','ibiasini5a@desdev.cn','Manila City','Philippines');
+INSERT INTO customers VALUES( 60,'Gregory','Loving','Male','gloving5h@mozilla.com','Taguig City','Philippines');
+INSERT INTO customers VALUES( 61,'Amy','Fridd','Female','afridd5k@hexun.com','Pasig City','Philippines');
+INSERT INTO customers VALUES( 62,'Valentine','Dranfield','Female','vdranfield5l@123 - reg.co.uk','Taguig City','Philippines');
+INSERT INTO customers VALUES( 63,'Kakalina','Mannix','Female','kmannix5t@apple.com','Makati City','Philippines');
+INSERT INTO customers VALUES( 64,'Anthony','Wailes','Male','awailes5u@wikimedia.org','Quezon City','Philippines');
+INSERT INTO customers VALUES( 65,'Lloyd','Brounsell','Male','lbrounsell5z@ox.ac.uk','Marikina City','Philippines');
+INSERT INTO customers VALUES( 66,'Sofie','Belly','Female','sbelly6c@studiopress.com','Pasig City','Philippines');
+INSERT INTO customers VALUES( 67,'Talyah','Rugieri','Female','trugieri6o@go.com','Makati City','Philippines');
+INSERT INTO customers VALUES( 68,'Kylila','Delia','Female','kdelia6t@altervista.org','Manila City','Philippines');
+INSERT INTO customers VALUES( 69,'Krista','Andresser','Female','kandresser6v@state.gov','Quezon City','Philippines');
+INSERT INTO customers VALUES( 70,'Aidan','Charleston','Female','acharleston6w@diigo.com','Mandaluyong City','Philippines');
+INSERT INTO customers VALUES( 71,'Belvia','Matteoni','Female','bmatteoni73@cocolog - nifty.com','Taguig City','Philippines');
+INSERT INTO customers VALUES( 72,'Elvina','Paffot','Female','epaffot74@nyu.edu','Marikina City','Philippines');
+INSERT INTO customers VALUES( 73,'Gaston','Durnan','Male','gdurnan78@amazon.de','San Juan City','Philippines');
+INSERT INTO customers VALUES( 74,'Lorilyn','Regan', 
+    'Female', 
+    'loregan7c@omniture.com', 
+    'Pasay City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    75, 'Alexandrina', 'Ceely', 'Female', 
+    'aceely7e@hibu.com', 'Mandaluyong City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    76, 'Cosmo', 'Wise', 'Male', 'cwise7i@mysql.com', 
+    'Taguig City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    77, 'Daisi', 'Dongall', 'Female', 'ddongall7n@usgs.gov', 
+    'Quezon City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    78, 'Claudelle', 'Spellard', 'Female', 
+    'cspellard7s@yahoo.com', 'Pasig City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    79, 'Erhart', 'Perazzo', 'Male', 'eperazzo7t@hexun.com', 
+    'Mandaluyong City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    80, 'Amii', 'Pestell', 'Female', 'apestell7x@yahoo.com', 
+    'Makati City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    81, 'Jesselyn', 'Albertson', 'Female', 
+    'jalbertson81@sakura.ne.jp', 'Manila City', 
+    'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    82, 'Putnam', 'Adger', 'Male', 'padger83@loc.gov', 
+    'Pasig City', 'Philippines'
+  );
+
+INSERT INTO customers VALUES (
+    83, 
+    'Ber', 
+    'OScollee','Male','boscollee85@people.com.cn','Makati City','Philippines');
+INSERT INTO customers VALUES( 84,'Allianora','Edmans','Female','aedmans89@bloglovin.com','Quezon City','Philippines');
+INSERT INTO customers VALUES( 85,'Daven','Douty','Male','ddouty8b@theatlantic.com','Pasig City','Philippines');
+INSERT INTO customers VALUES( 86,'Jereme','Doogood','Male','jdoogood8c@devhub.com','Taguig City','Philippines');
+INSERT INTO customers VALUES( 87,'Reba','Dupoy','Female','rdupoy8f@canalblog.com','Manila City','Philippines');
+INSERT INTO customers VALUES( 88,'Juliane','Rushforth','Female','jrushforth8n@icio.us','Pasig City','Philippines');
+INSERT INTO customers VALUES( 89,'Laurel','Bly','Female','lbly8t@kickstarter.com','Makati City','Philippines');
+INSERT INTO customers VALUES( 90,'Rubie','Dunwoody','Female','rdunwoody8v@auda.org.au','Mandaluyong City','Philippines');
+INSERT INTO customers VALUES( 91,'Casey','Mayer','Male','cmayer93@nsw.gov.au','Pasig City','Philippines');
+INSERT INTO customers VALUES( 92,'Penrod','Oade','Male','poade94@soundcloud.com','Quezon City','Philippines');
+INSERT INTO customers VALUES( 93,'Sydney','Fransinelli','Male','sfransinelli96@google.pl','Manila City','Philippines');
+INSERT INTO customers VALUES( 94,'Honey','Picott','Female','hpicott9g@123 - reg.co.uk','San Juan City','Philippines');
+INSERT INTO customers VALUES( 95,'Darrick','Mapham','Male','dmapham9h@nih.gov','Pasay City','Philippines');
+INSERT INTO customers VALUES( 96,'Enriqueta','Selway','Female','eselway9k@msn.com','Pasig City','Philippines');
+INSERT INTO customers VALUES( 97,'Marlane','Alliott','Female','malliott9q@ovh.net','Quezon City','Philippines');
+INSERT INTO customers VALUES( 98,'Oates','Ronisch','Male','oronisch9r@bravesites.com','Pasig City','Philippines');
+INSERT INTO customers VALUES( 99,'Win','Kacheler','Male','wkacheler9v@t.co','Quezon City','Philippines');
+INSERT INTO customers VALUES( 100,'Candis','Pittock','Female','cpittocka3@independent.co.uk','Quezon City','Philippines');
---
+++ products.sql
@@ -0,0 +1,221 @@
+﻿/* CREATE TABLE */
+CREATE TABLE products(
+  product_id INT, 
+  name VARCHAR(100)
+);
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (1, 'Soup Bowl Clear 8oz92008');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (2, 'Hersey Shakes');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (3, 'Corn Meal');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (4, 'Orange - Canned, Mandarin');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (5, 'Appetizer - Sausage Rolls');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (
+    6, 'Wine - Carmenere Casillero Del'
+  );
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (7, 'Butter Sweet');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (8, 'Irish Cream - Butterscotch');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (9, 'Haggis');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (10, 'Lamb - Whole, Frozen');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (11, 'Wine - Redchard Merritt');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (12, 'Salt - Kosher');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (13, 'Flour Dark Rye');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (
+    14, 'Nantucket - Kiwi Berry Cktl.'
+  );
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (15, 'Bay Leaf Ground');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (16, 'Chocolate - Milk Coating');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (17, 'Cheese - Asiago');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (18, 'Salami - Genova');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (19, 'Cheese - Cambozola');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (20, 'Plasticforkblack');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (21, 'Pop Shoppe Cream Soda');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (22, 'Wine - Taylors Reserve');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (23, 'Ecolab - Medallion');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (24, 'Red Cod Fillets - 225g');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (25, 'Plastic Arrow Stir Stick');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (26, 'Vodka - Moskovskaya');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (27, 'Pants Custom Dry Clean');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (
+    28, 'Beer - Paulaner Hefeweisse'
+  );
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (29, 'Eggplant - Regular');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (
+    30, 'Wine - Ej Gallo Sierra Valley'
+  );
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (
+    31, 'Lettuce - Baby Salad Greens'
+  );
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (32, 'Wasabi Powder');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (33, 'Tart Shells - Sweet, 2');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (34, 'Easy Off Oven Cleaner');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (35, 'Beef - Chuck, Boneless');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (
+    36, 'Wine - White, Pelee Island'
+  );
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (
+    37, 'Soup Campbells Mexicali Tortilla'
+  );
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (
+    38, 'Bowl 12 Oz - Showcase 92012'
+  );
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (39, 'Veal - Inside, Choice');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (40, 'Cup - Paper 10oz 92959');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (41, 'Lettuce - Iceberg');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (42, 'Mussels - Frozen');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (43, 'Broom - Corn');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (44, 'Seaweed Green Sheets');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (45, 'Steampan - Foil');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (46, 'Compound - Strawberry');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (47, 'Pork Loin Cutlets');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (48, 'Anchovy Paste - 56 G Tube');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (49, 'Beans - Navy, Dry');
+
+INSERT INTO products(product_id, name) 
+VALUES 
+  (50, 'Icecream - Dibs');
